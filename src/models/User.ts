7be3794e3339c2192bf12dc94be8e159c32d4bb6
skinnyfads <skinnyfads@proton.me>
2023-01-16 18:20:09 +0700

    feat(database): unique username

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 const User = model("user", userSchema);
